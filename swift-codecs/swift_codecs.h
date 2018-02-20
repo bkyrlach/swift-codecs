02dd3052fbe16655044b2fd6a496bee9efd30b4e
Ben Kyrlach <bkyrlach@gmail.com>
2018-02-19 19:58:56 -0500

    Initial Commit

---
+++ swift-codecs/swift_codecs.h
@@ -0,0 +1,19 @@
+//
+//  swift_codecs.h
+//  swift-codecs
+//
+//  Created by Benjamin Kyrlach on 2/19/18.
+//  Copyright © 2018 Benjamin Kyrlach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift_codecs.
+FOUNDATION_EXPORT double swift_codecsVersionNumber;
+
+//! Project version string for swift_codecs.
+FOUNDATION_EXPORT const unsigned char swift_codecsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_codecs/PublicHeader.h>
+
+
